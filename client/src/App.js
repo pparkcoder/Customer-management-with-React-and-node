052c2c5818e660931d146a1cab8e727b64ab9128
pparkcoder <pparkcoder@gmail.com>
2021-08-31 00:01:52 +0900

    이미지 업로드 기능 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import CustomerAdd from './components/CustomerAdd';
 import Paper from '@material-ui/core/Paper'
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
@@ -26,14 +27,25 @@ const styles = theme => ({
 
 class App extends Component {
 
-    state = {
-      customers: "",
-      completed : 0
+    constructor(props){
+      super(props);
+      this.state = {
+        customers : '',
+        completed : 0
+      }
     }
-
+    stateRefresh = () => {
+      this.setState({
+        customers : '',
+        completed : 0
+      });
+      this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+    }
     componentDidMount(){
       this.timer = setInterval(this.progress, 110); //0.02초마다 progress 함수 실행
-      this.callApi()
+      this.callApi()  // 고객 데이터를 불러옴
       .then(res => this.setState({customers: res}))
       .catch(err => console.log(err));
     }
@@ -51,42 +63,45 @@ class App extends Component {
     render() {
     const {classes} = this.props;
     return (
-      <Paper className = {classes.root}>
-        <Table className = {classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-          {
-            this.state.customers ? this.state.customers.map(c => {
-              return(
-                <Customer
-                  key = {c.id}
-                  id = {c.id}
-                  image = {c.image}
-                  name = {c.name}
-                  birthday = {c.birthday}
-                  gender = {c.gender}
-                  job = {c.job}
-                  />
-              );
-            }) :
-            <TableRow>
-              <TableCell colSpan = '6' align = "center">
-                <CircularProgress className = {classes.progress} variant = "determinate" value = {this.state.completed}/>
-              </TableCell>
-            </TableRow>
-          }
-          </TableBody>
-        </Table>
-    </Paper>
+      <div>
+        <Paper className = {classes.root}>
+          <Table className = {classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+            {
+              this.state.customers ? this.state.customers.map(c => {
+                return(
+                  <Customer
+                    key = {c.id}
+                    id = {c.id}
+                    image = {c.image}
+                    name = {c.name}
+                    birthday = {c.birthday}
+                    gender = {c.gender}
+                    job = {c.job}
+                    />
+                );
+              }) :
+              <TableRow>
+                <TableCell colSpan = '6' align = "center">
+                  <CircularProgress className = {classes.progress} variant = "determinate" value = {this.state.completed}/>
+                </TableCell>
+              </TableRow>
+            }
+            </TableBody>
+          </Table>
+      </Paper>
+      <CustomerAdd stateRefresh = {this.stateRefresh}/>
+    </div>
     );
   }
 }
